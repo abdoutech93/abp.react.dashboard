7a023bc2d09b883118bad4022c87ba9333857565
SOAL Abdeldjallil <abdeldjallil93@hotmail.fr>
2023-07-25 17:28:52 +0100

    Initial commit

---
+++ src/lib/abpUtility.ts
@@ -0,0 +1,14 @@
+import AppConsts from './appconst';
+
+declare var abp: any;
+
+export function L(key: string, sourceName?: string): string {
+  let localizationSourceName = AppConsts.localization.defaultLocalizationSourceName;
+  if (abp.localization) {
+    return abp.localization.localize(key, sourceName ? sourceName : localizationSourceName);
+  }
+}
+
+export function isGranted(permissionName: string): boolean {
+  return abp.auth.isGranted(permissionName);
+}
